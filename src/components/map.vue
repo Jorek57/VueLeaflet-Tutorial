db7cb0b6279ee5f0671054718b20206b6c9b5081
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2020-05-11 20:45:49 +0200

    Example 3: Markers on map

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,40 +1,66 @@
 <template>
-    <div>
-        <l-map
-            class="map"
-            ref="map"
-        >
-            <l-tile-layer
-                :url="url"
-            >
-            </l-tile-layer>
-        </l-map>
-        <h1>Here comes the map</h1>
-    </div>
+  <l-map
+    :center="center"
+    :zoom="zoom"
+    class="map"
+    ref="map"
+    @update:zoom="zoomUpdated"
+    @update:center="centerUpdated"
+  >
+    <l-tile-layer
+      :url="url"
+    >
+    </l-tile-layer>
+    <l-marker
+      v-for="marker in markers"
+      :key="marker.id"
+      :lat-lng="marker.coordinates"
+    >
+    </l-marker>
+  </l-map>
 </template>
 
 <script>
-import { LMap, LTileLayer, } from 'vue2-leaflet';
-import 'leaflet/dist/leaflet.css';
+  import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+  import 'leaflet/dist/leaflet.css';
 
-export default {
+  export default {
     components: {
-        LMap,
-        LTileLayer,
+      LMap,
+      LTileLayer,
+      LMarker
     },
     data () {
-        return {
-            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-        }
+      return {
+        url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+        center: [ 49.1193089, 6.1757156 ],
+        zoom: 12,
+        markers: [
+          {id: 1, coordinates: [ 49.114910, 6.178810 ]},
+          {id: 2, coordinates: [ 49.133290, 6.154370 ]},
+          {id: 3, coordinates: [ 49.102160, 6.158850 ]},
+          {id: 4, coordinates: [ 49.136010, 6.199630 ]},
+          {id: 5, coordinates: [ 49.105563, 6.182234 ]},
+        ]
+      }
+    },
+    methods: {
+      zoomUpdated (zoom) {
+        this.zoom = zoom;
+        console.log(this.markers)
+      },
+      centerUpdated (center) {
+        this.center = center;
+      }
     }
-}
+  }
 </script>
 
 <style>
-    .map {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        overflow :hidden
-    }
+  .map {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    overflow :hidden
+  }
 </style>
