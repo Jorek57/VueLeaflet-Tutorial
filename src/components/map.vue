60908de0889da7a63c20c1f283dc4c46e6aa0044
Kevin Cluzel <kevin.cluzel@hotmail.fr>
2020-05-11 18:42:28 +0200

    Example 2: Map appears

--- src/components/map.vue
+++ src/components/map.vue
@@ -1,11 +1,40 @@
 <template>
     <div>
+        <l-map
+            class="map"
+            ref="map"
+        >
+            <l-tile-layer
+                :url="url"
+            >
+            </l-tile-layer>
+        </l-map>
         <h1>Here comes the map</h1>
     </div>
 </template>
 
 <script>
+import { LMap, LTileLayer, } from 'vue2-leaflet';
+import 'leaflet/dist/leaflet.css';
+
+export default {
+    components: {
+        LMap,
+        LTileLayer,
+    },
+    data () {
+        return {
+            url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        }
+    }
+}
 </script>
 
-<style scoped>
+<style>
+    .map {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        overflow :hidden
+    }
 </style>
